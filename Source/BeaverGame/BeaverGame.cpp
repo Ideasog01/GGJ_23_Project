2d3fb9653abe83dc2c608e535264e5733dd74e43
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2023-01-30 19:54:54 +0000

    Initial Commit

---
+++ Source/BeaverGame/BeaverGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BeaverGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeaverGame, "BeaverGame" );

fddb19dbcbd85e87624a6025a4407ae3c3f8afc2
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2023-01-30 20:17:33 +0000

    Classes

---
+++ Source/BeaverGame/Controllers/BeaverPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BeaverPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEAVERGAME_API ABeaverPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
